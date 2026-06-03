679f4a608b5f4e18fb14d96e783b38c2deba77ca
VishakhaKaushal <vishakhakaushal050820@gmail.com>
2026-06-03 15:38:37 +0530

    Fix WebSocketAuthConfig import

--- Backend/src/main/java/com/ideaspark/config/WebSocketAuthConfig.java
+++ Backend/src/main/java/com/ideaspark/config/WebSocketAuthConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.config.ChannelRegistration;
-import org.springframework.messaging.simp.config.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
