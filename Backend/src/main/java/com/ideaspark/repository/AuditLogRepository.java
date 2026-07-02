17eddd321cd9e871ffa79b17d4d6d6e0ef97cb07
THRIPURARIBHATLA V N S APARNA <tvnsaparna@gmail.com>
2026-07-02 16:32:13 +0530

    Change AuditLogRepository ID type from Long to UUID

--- Backend/src/main/java/com/ideaspark/repository/AuditLogRepository.java
+++ Backend/src/main/java/com/ideaspark/repository/AuditLogRepository.java
@@ -4,6 +4,8 @@ import com.ideaspark.model.AuditLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
+public interface AuditLogRepository extends JpaRepository<AuditLog, UUID> {
 }
